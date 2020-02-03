af288acee1f4d0bbb5ba0c1826a033eb2bd416dd
Alberto Piai <apiai@sensational.ch>
2020-02-03 14:50:44 +0100

    minimal repro
    fix up the paths to your android ndk installation

---
+++ c/main.c
@@ -0,0 +1,7 @@
+#include <stdint.h>
+
+void foo(uint32_t x);
+
+void bar(void) {
+    foo(42);
+}
---
+++ repro.sh
@@ -0,0 +1,44 @@
+#!/bin/bash
+
+set -eu
+set -o pipefail
+
+ANDROID_NDK=/Users/albe/Library/Android/sdk/ndk/20.0.5594570
+
+echo
+echo "*** Running with ANDROID_NDK=$ANDROID_NDK"
+echo
+
+build() {
+cargo build --target i686-linux-android
+
+"$ANDROID_NDK"/toolchains/llvm/prebuilt/darwin-x86_64/bin/i686-linux-android23-clang c/main.c -c -o target/main.o
+
+"$ANDROID_NDK"/toolchains/llvm/prebuilt/darwin-x86_64/i686-linux-android/bin/ld \
+    -shared -o foo.so -soname foo.so \
+    --warn-shared-textrel --fatal-warnings \
+    target/main.o \
+    target/i686-linux-android/debug/librepro.a
+}
+
+
+cargo clean
+
+rustup default 1.40.0
+
+rustc --version
+
+build
+
+printf "\n\n*** Everything's fine on 1.40.0 ***\n\n"
+
+
+cargo clean
+
+rustup default stable
+
+rustc --version
+
+build # this fails
+
+printf "\n\n*** Everything's fine on stable ***\n\n"  ## Nope, doesn't get here
